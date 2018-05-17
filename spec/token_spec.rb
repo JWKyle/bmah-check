2b13eca05d50299b3a3ca403045b5404711c7967
Jim Kyle <jim@jwkyle.com>
2018-05-17 11:35:23 -0500

    add tests for refresh and current_price, tests now green

---
+++ spec/token_spec.rb
@@ -0,0 +1,20 @@
+RSpec.describe Token do
+  describe "#refresh" do
+    it "Pulls the Current Wow Token Price from Battle.Net API" do
+      expect(Token.refresh).to be_kind_of(Integer)
+    end
+  end
+  describe "#current_price" do
+    it "Shows the formated price as a  string" do
+      expect(Token.current_price).to be_kind_of(String)
+    end
+
+    it "Shows the formated price ending with 'g'" do
+      expect(Token.current_price).to end_with('g')
+    end
+  #
+  #   pending "Shows printout of bidding information" do
+  #     expect(Check.current_auction).to eq("")
+  #   end
+  end
+end
